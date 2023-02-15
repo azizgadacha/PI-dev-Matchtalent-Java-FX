901c86dd921562579421291e8adb652935064522
aziz.gadacha <aziz.gadacha.2000@gmail.com>
2023-02-15 23:47:18 +0100

    rectifation

--- src/services/IService.java
+++ src/services/IService.java
@@ -15,5 +15,5 @@ public interface IService<T> {
     void ajouter(T t)throws SQLException;
     void modifier(T t)throws SQLException;
     void supprimer(T t)throws SQLException;
-    List<T> secuperer()throws SQLException;
+    List<T> recuperer()throws SQLException;
 }
